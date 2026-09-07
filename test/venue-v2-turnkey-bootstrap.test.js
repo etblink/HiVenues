817480d9afc534eb21a4d09ffbe5481da75fe9a6
Evan Kotler <etblink@users.noreply.github.com>
2026-09-07 14:56:22 -0700

    Import URLSearchParams in v2 bootstrap test

--- test/venue-v2-turnkey-bootstrap.test.js
+++ test/venue-v2-turnkey-bootstrap.test.js
@@ -4,6 +4,7 @@ const assert = require('node:assert/strict');
 const fs = require('node:fs');
 const os = require('node:os');
 const path = require('node:path');
+const { URLSearchParams } = require('node:url');
 const test = require('node:test');
 
 const {
