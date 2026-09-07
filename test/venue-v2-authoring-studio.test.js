096ed6dbdfe4a53bc9f01ff1e6d64c74c0a185d4
Evan Kotler <etblink@users.noreply.github.com>
2026-09-07 00:17:21 -0700

    Fix v2 authoring URLSearchParams imports

--- test/venue-v2-authoring-studio.test.js
+++ test/venue-v2-authoring-studio.test.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const { URLSearchParams } = require('node:url');
+
 const assert = require('node:assert/strict');
 const test = require('node:test');
 const request = require('supertest');
