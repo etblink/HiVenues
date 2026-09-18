2d7970a948edd4a7a690826d4635ef3e50d49afc
Evan Kotler <etblink@users.noreply.github.com>
2026-09-18 15:08:49 -0700

    cleanup: remove superseded milestone and deployment tooling

--- scripts/check-hivenues-v1-release.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-'use strict';
-
-// Deprecated command name retained temporarily so existing local workflows fail
-// cleanly into the renamed wiring check. This command does not declare product V1.
-require('./check-turnkey-wiring');
