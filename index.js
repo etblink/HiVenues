f545b2d9f2a320c09d928d726a694c6040b976cf
Evan Kotler <etblink@users.noreply.github.com>
2026-09-18 15:07:30 -0700

    cleanup: remove superseded application stacks and dead shared modules

--- index.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const { createApp } = require('./src/app');
-const { startServer } = require('./src/server');
-
-if (require.main === module) startServer();
-
-module.exports = { createApp, startServer };
