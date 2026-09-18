21889f3b05ce56d098db0dfe5b5e208f0665bdcc
Evan Kotler <etblink@users.noreply.github.com>
2026-09-17 23:54:28 -0700

    test: use renderable synthetic profile images

--- scripts/product-browser-qualification.js
+++ scripts/product-browser-qualification.js
@@ -192,7 +192,7 @@ function productReadService(publicKey) {
           etblink: {
             name: 'etblink',
             displayName: 'Evan',
-            profileImage: 'data:image/svg+xml,%3Csvg/%3E',
+            profileImage: 'data:image/svg+xml,%3Csvg xmlns=%22http://www.w3.org/2000/svg%22 width=%221%22 height=%221%22/%3E',
           },
         },
         nextCursor: null,
@@ -209,7 +209,7 @@ function productReadService(publicKey) {
         name,
         displayName: name,
         about: '',
-        profileImage: 'data:image/svg+xml,%3Csvg/%3E',
+        profileImage: 'data:image/svg+xml,%3Csvg xmlns=%22http://www.w3.org/2000/svg%22 width=%221%22 height=%221%22/%3E',
       }]));
     },
     async getProfile(account) {
